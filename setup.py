9cdc61d1fd0c05d97ddb76c5f246062d952f0d5d
mnorbury <martin.norbury@gmail.com>
2013-08-14 14:11:27 -0700

    Changed to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "lcogt-dateutils",
-        version = "0.1.0",
+        version = "0.1.1",
         description = "Utility functions for manipulating datetimes",
         author = "Martin Norbury",
         author_email = "mnorbury@lcogt.net",
